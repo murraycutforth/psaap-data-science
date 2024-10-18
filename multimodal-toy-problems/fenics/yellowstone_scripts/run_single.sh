98fae2dff2abd306e6ddd87cfce460e50e24db11
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-10-18 15:50:45 -0700

    YS test script

---
+++ multimodal-toy-problems/fenics/yellowstone_scripts/run_single.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#sbatch -j fenicsx-debug               # job name
+#sbatch -n 1                  # total number of nodes requested
+#sbatch -t 1:00:00           # run time (hh:mm:ss)
+#sbatch -p cpu
+
+date
+
+# activate python env
+eval "$(/opt/ohpc/pub/compiler/anaconda3/2024.02-1/bin/conda shell.bash hook)"
+conda activate fenicsx-env
+
+# run main script
+cd /home/darve/mcc4/psaap-data-science/multimodal-toy-problems/fenics
+miprun -n 32 python flow_past_cylinder.py --Re 100
+
+date
