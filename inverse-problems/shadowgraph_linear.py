b3359c248435c7322d5295e0a00302fe14c8caab
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-10-20 16:39:02 -0700

    Code for linear model of shaowgraphy

---
+++ inverse-problems/shadowgraph_linear.py
@@ -0,0 +1,202 @@
+import unittest
+
+import numpy as np
+import einops
+
+
+def ind_to_3d(i, j, k, Nx, Ny, Nz) -> int:
+    """C-style ordering of 3D indices
+    Equivalent to einops.rearrange(x, 'i j k -> (i j k)')
+    """
+    return i * Ny * Nz + j * Nz + k
+
+
+def integrate_z_operator(Nx, Ny, Nz) -> np.array:
+    """Construct the integration operator for the z axis
+    """
+    A = np.zeros((Nx * Ny, Nx * Ny * Nz))
+    for i in range(Nx * Ny):
+        for j in range(Nx * Ny * Nz):
+            k = j // Nz
+            A[i, j] = 1 if k == i else 0
+    return A
+
+
+def grad_2_x_operator(Nx, Ny, Nz, dx = 1.0) -> np.array:
+    """Construct the second derivative operator for the x axis
+    """
+    assert Nx >= 3
+    A = np.zeros((Nx * Ny * Nz, Nx * Ny * Nz))
+
+    # i=0 BC, covers elements [0, Ny * Nz) in output vector
+    for i in range(Ny * Nz):
+        A[i, i] = -1
+        A[i, i + Ny * Nz] = 1
+
+    # i=Nx-1 BC, covers elements [(Nx - 1) * Ny * Nz, Nx * Ny * Nz) in output vector
+    for i in range((Nx - 1) * Ny * Nz, Nx * Ny * Nz):
+        A[i, i] = -1
+        A[i, i - Ny * Nz] = 1
+
+    # Interior points
+    for i in range(Ny * Nz, (Nx - 1) * Ny * Nz):
+        A[i, i - Ny * Nz] = 1
+        A[i, i] = -2
+        A[i, i + Ny * Nz] = 1
+
+    return A / dx ** 2
+
+
+def grad_2_y_operator(Nx, Ny, Nz, dy = 1.0) -> np.array:
+    """Construct the second derivative operator for the y axis
+    """
+    assert Ny >= 3
+    A = np.zeros((Nx * Ny * Nz, Nx * Ny * Nz))
+
+    bc_inds = set()
+
+    # j=0 BC, covers elements i * Nx * Nz + k for all i, k
+    for i in range(Nx):
+        for j in range(Nz):
+            vec_ind = ind_to_3d(i, 0, j, Nx, Ny, Nz)
+            bc_inds.add(vec_ind)
+            A[vec_ind, vec_ind] = -1
+            A[vec_ind, vec_ind + Nz] = 1
+
+    # j=Ny-1 BC, covers elements i * Nx * Nz + k for all i, k
+    for i in range(Nx):
+        for j in range(Nz):
+            vec_ind = ind_to_3d(i, Ny - 1, j, Nx, Ny, Nz)
+            bc_inds.add(vec_ind)
+            A[vec_ind, vec_ind] = -1
+            A[vec_ind, vec_ind - Nz] = 1
+
+    # Interior points
+    for i in range(Nx * Ny * Nz):
+        if i not in bc_inds:
+            A[i, i - Nz] = 1
+            A[i, i] = -2
+            A[i, i + Nz] = 1
+
+    return A / dy ** 2
+
+
+class TestOperators(unittest.TestCase):
+    def setUp(self):
+        # Constant volume
+        self.ones_vol = np.ones((2, 2, 2))
+        self.ones_vec = einops.rearrange(self.ones_vol, 'i j k -> (i j k)')
+
+        # Volume in which elements increase linearly in x direction
+        self.N = 5
+        self.x_linear_vol = np.zeros((self.N, self.N, self.N))
+        for i in range(self.N):
+            self.x_linear_vol[i, :, :] = i
+        self.x_linear_vec = einops.rearrange(self.x_linear_vol, 'i j k -> (i j k)')
+
+        # Volume in which elements increase as i**2 in x direction
+        self.x_quad_vol = np.zeros((self.N, self.N, self.N))
+        for i in range(self.N):
+            self.x_quad_vol[i, :, :] = i ** 2
+        self.x_quad_vec = einops.rearrange(self.x_quad_vol, 'i j k -> (i j k)')
+
+        # Volume in which elements increase as j**2 in y direction
+        self.y_quad_vol = np.zeros((self.N, self.N, self.N))
+        for j in range(self.N):
+            self.y_quad_vol[:, j, :] = j ** 2
+        self.y_quad_vec = einops.rearrange(self.y_quad_vol, 'i j k -> (i j k)')
+
+    def test_ind_to_3d(self):
+        self.assertEqual(ind_to_3d(0, 0, 0, 10, 10, 10), 0)
+        self.assertEqual(ind_to_3d(0, 0, 1, 10, 10, 10), 1)
+        self.assertEqual(ind_to_3d(0, 1, 0, 10, 10, 10), 10)
+        self.assertEqual(ind_to_3d(1, 0, 0, 10, 10, 10), 100)
+
+    def test_integrate_z_operator_0(self):
+        A = integrate_z_operator(2, 2, 2)
+        self.assertEqual(A.shape, (4, 8))
+        self.assertTrue(np.allclose(A.sum(axis=0), np.array([1, 1, 1, 1, 1, 1, 1, 1])))
+        result = A @ self.ones_vec
+        self.assertTrue(np.allclose(result, einops.rearrange(self.ones_vol.sum(axis=2), 'i j -> (i j)')))
+        self.assertEqual(result.shape, (4,))
+
+    def test_integrate_z_operator_1(self):
+        A = integrate_z_operator(self.N, self.N, self.N)
+        result = A @ self.x_linear_vec
+        self.assertTrue(np.allclose(result, einops.rearrange(self.x_linear_vol.sum(axis=2), 'i j -> (i j)')))
+
+    def test_integrate_z_operator_large(self):
+        Nx, Ny, Nz = 4, 4, 4
+        vol = np.ones((Nx, Ny, Nz))
+        vec = einops.rearrange(vol, 'i j k -> (i j k)')
+        A = integrate_z_operator(Nx, Ny, Nz)
+        result = A @ vec
+        self.assertTrue(np.allclose(result, einops.rearrange(vol.sum(axis=2), 'i j -> (i j)')))
+
+    def test_integrate_z_operator_non_cubic(self):
+        Nx, Ny, Nz = 3, 2, 4
+        vol = np.ones((Nx, Ny, Nz))
+        vec = einops.rearrange(vol, 'i j k -> (i j k)')
+        A = integrate_z_operator(Nx, Ny, Nz)
+        result = A @ vec
+        self.assertTrue(np.allclose(result, einops.rearrange(vol.sum(axis=2), 'i j -> (i j)')))
+
+    def test_integrate_z_operator_single_z(self):
+        Nx, Ny, Nz = 3, 3, 1
+        vol = np.ones((Nx, Ny, Nz))
+        vec = einops.rearrange(vol, 'i j k -> (i j k)')
+        A = integrate_z_operator(Nx, Ny, Nz)
+        result = A @ vec
+        self.assertTrue(np.allclose(result, einops.rearrange(vol.sum(axis=2), 'i j -> (i j)')))
+
+    def test_grad_2_x_operator_0(self):
+        A = grad_2_x_operator(3, 3, 3)
+        self.assertEqual(A.shape, (27, 27))
+        self.assertTrue(np.allclose(A.sum(axis=0), np.zeros((27,))))
+        result = A @ np.ones((27,))
+        self.assertTrue(np.allclose(result, np.zeros((27,))))
+
+    def test_grad_2_x_operator_1(self):
+        A = grad_2_x_operator(self.N, self.N, self.N)
+        result = A @ self.x_linear_vec
+        # First 25 elements should be 1, middle 0, last 25 -1
+        self.assertTrue(np.allclose(result[:25], np.ones((25,))))
+        self.assertTrue(np.allclose(result[25:-25], np.zeros((self.N * self.N * self.N - 50,))))
+        self.assertTrue(np.allclose(result[-25:], -np.ones((25,))))
+
+    def test_grad_2_x_operator_2(self):
+        A = grad_2_x_operator(self.N, self.N, self.N)
+        result = A @ self.x_quad_vec
+        # First 25 elements should be 1, middle 2, last 25 -7
+        self.assertTrue(np.allclose(result[:25], 1 * np.ones((25,))))
+        self.assertTrue(np.allclose(result[25:-25], 2 * np.ones((self.N * self.N * self.N - 50,))))
+        self.assertTrue(np.allclose(result[-25:], -7 * np.ones((25,))))
+
+    def test_grad_2_x_operator_3(self):
+        # Check that operator predicts zero for y_quad_vol
+        A = grad_2_x_operator(self.N, self.N, self.N)
+        result = A @ self.y_quad_vec
+        self.assertTrue(np.allclose(result, np.zeros((self.N * self.N * self.N,))))
+
+    def test_grad_2_y_operator_shape(self):
+        A = grad_2_y_operator(self.N, self.N, self.N)
+        self.assertEqual(A.shape, (self.N * self.N * self.N, self.N * self.N * self.N))
+
+    def test_grad_2_y_operator_boundary_conditions(self):
+        A = grad_2_y_operator(self.N, self.N, self.N)
+        result = A @ np.ones((self.N * self.N * self.N,))
+        self.assertTrue(np.allclose(result, np.zeros((self.N * self.N * self.N,))))
+
+    def test_grad_2_y_operator_quadratic(self):
+        A = grad_2_y_operator(self.N, self.N, self.N)
+        result = A @ self.y_quad_vec
+        # All elements should either be 2, -7, or 1
+        unique = set(np.unique(result))
+        self.assertEqual(len(unique), 3)
+        self.assertTrue(np.allclose(sorted(unique), [-7, 1, 2]))
+
+    def test_grad_2_y_operator_zero_for_x_quad(self):
+        A = grad_2_y_operator(self.N, self.N, self.N)
+        result = A @ self.x_quad_vec
+        self.assertTrue(np.allclose(result, np.zeros((self.N * self.N * self.N,))))
+
---
+++ inverse-problems/shadowgraph_linear_debug.py
@@ -0,0 +1,105 @@
+# In this script, we play around with a linear forward model for shadowgraphy
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from plotting_utils import vol_and_im_plot
+
+def main():
+
+    I_0 = 1
+    c = -1
+    dx = 2 / 100
+    dy = 2 / 100
+    dz = 2 / 100
+
+    arr = construct_psaap_density_example()
+    I = forward_model(I_0, c, arr, dx, dy, dz)
+
+    vol_and_im_plot(arr, I)
+
+    assert 0
+    plt.imshow(I[2:, 20:140], cmap='gray')
+    plt.colorbar()
+    plt.show()
+
+    arr = construct_n_circular_shock(100, 100, 100, 0.5, 0.9, 1.1)
+    I = forward_model(I_0, c, arr, dx, dy, dz)
+
+    plt.imshow(I)
+    plt.colorbar()
+    plt.show()
+
+    arr = construct_n_gaussian(100, 100, 100, 0.1, 0.1, 1)
+    I = forward_model(I_0, c, arr, dx, dy, dz)
+
+    plt.imshow(I)
+    plt.colorbar()
+    plt.show()
+
+
+def forward_model(I_0, c, n_arr, dx, dy, dz):
+    """Linear forward model for shadowgraphy
+    """
+    grad_2_x_n = grad_2_x(n_arr, dx)
+    grad_2_y_n = grad_2_y(n_arr, dy)
+    return I_0 + c * int_z(grad_2_x_n + grad_2_y_n, dz)
+
+
+def grad_2_x(arr, dx):
+    """Compute the second derivative of arr with respect to x
+    """
+    arr_padded = np.pad(arr, pad_width=((1, 1), (0, 0), (0, 0)), mode='edge')  # Edge gives zero gradient on the boundaries
+    grad_2_x_arr = (arr_padded[:-2, :, :] - 2 * arr_padded[1:-1, :, :] + arr_padded[2:, :, :]) / dx ** 2
+    assert grad_2_x_arr.shape == arr.shape
+    return grad_2_x_arr
+
+
+def grad_2_y(arr, dy):
+    """Compute the second derivative of arr with respect to y
+    """
+    arr_padded = np.pad(arr, ((0, 0), (1, 1), (0, 0)), mode='edge')
+    grad_2_y_arr = (arr_padded[:, :-2, :] - 2 * arr_padded[:, 1:-1, :] + arr_padded[:, 2:, :]) / dy ** 2
+    assert grad_2_y_arr.shape == arr.shape
+    return grad_2_y_arr
+
+
+def int_z(arr, dz):
+    """Estimate integral over z axis using trapezium rule
+    """
+    return 0.5 * dz * (np.sum(arr[:, :, 1:], axis=2) + np.sum(arr[:, :, :-1], axis=2))
+    #return np.trapz(arr, dx=dz, axis=2)
+
+
+def construct_n_gaussian(Nx, Ny, Nz, std, A, B):
+    """Construct a 3D Gaussian with standard deviation std
+    """
+    x = np.linspace(-1, 1, Nx)
+    y = np.linspace(-1, 1, Ny)
+    z = np.linspace(-1, 1, Nz)
+    X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+    arr = B + A * np.exp(-(X ** 2 + Y ** 2 + Z ** 2) / (2 * std ** 2))
+    return arr
+
+
+def construct_n_circular_shock(Nx, Ny, Nz, R, n_inside, n_outside):
+    """Construct a 3D circular shock with radius R, constant in z direction
+    """
+    x = np.linspace(-1, 1, Nx)
+    y = np.linspace(-1, 1, Ny)
+    z = np.linspace(-1, 1, Nz)
+    X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+    arr = n_outside + n_inside * (np.sqrt(X ** 2 + Y ** 2) < R)
+    return arr
+
+
+def construct_psaap_density_example():
+    rho_path = '/Users/murray/Projects/density_generative_model/output/3d_fixed_subdomain_data/72_0.npz'
+    rho = np.load(rho_path)['rho']
+    return rho
+
+
+# TODO: assemble matrix A s.t. forward model is given by A @ u
+
+if __name__ == '__main__':
+    main()
